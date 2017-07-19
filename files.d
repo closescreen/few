63317d16bf1344921b51b494522054034b2e92c8
Close Screen <close.screen@gmail.com>
2017-07-19 03:07:20 +0300

    files.d

--- files.d
+++ files.d
@@ -1,5 +1,32 @@
-import std.datetime, std.string, std.regex, std.exception, std.conv, std.range;
+import std.datetime, std.string, std.regex, std.exception, std.conv, std.range, std.range, std.algorithm;
 
-//auto files( I<:Interval )( I  i){
-// return i;
-//}
+
+
+auto strftimeLike(TP)( TP tp, string fmt )
+if ( isTimePoint!TP ){
+ 
+ string repl( Captures!(string) m ){
+  switch (m[0])
+  {
+	case "%F": return tp.date.toISOExtString;
+	case "%H": return "%02d".format( tp.hour );
+	case "%Y": return "%d".format( tp.year);
+	case "%m": return "%02d".format( tp.month );
+	case "%d": return "%02d".format( tp.day );
+	case "%M": return "%02d".format( tp.minute );
+	case "%S": return "%02d".format( tp.second );
+	default: return m[0];
+  }
+ }
+ 
+ auto re = regex(`%[FHYmdMS]`,"g");
+ 
+ return fmt.replaceAll!repl(re);
+  	
+}
+
+auto files(string fmt, I)( I r)
+//if ( isTimePoint!TP )
+{
+ return r.map!( tp=>tp.strftimeLike( fmt ));
+}
